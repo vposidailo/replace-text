64a09752774f02dde448a4d141c9e34c4192cd32
Vitalii Posidailo <Vitalii.Posidailo@gds.ey.com>
2021-03-29 09:59:41 +0200

    Added replacement lab

---
+++ RegularReplacement/Replace.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace RegularReplacement
+{
+    public partial class Replace : Form
+    {
+        private const char WILD_CHARACTER = '%';
+
+        public Replace()
+        {
+            InitializeComponent();
+            this.CenterToParent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var sourceText = ((Text)Owner).GetText();
+            var startPosition = GetCursorPosition();
+            startPosition += ((Text)Owner).GetSelectedTextIndexEnd();
+
+            var searchText = textBox1.Text;
+            var regex = BuildSearchPattern(searchText);
+
+            foreach (var word in sourceText.Substring(startPosition).Split(' '))
+            {
+                if(regex.IsMatch(word))
+                {
+                    ((Text)Owner).HigthSelectedText(startPosition, word);
+                    return;
+                }
+
+                startPosition += word.Length;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var sourceText = ((Text)Owner).GetText();
+            var position = GetCursorPosition();
+            var transformationResultresult = new StringBuilder();
+
+            var searchText = textBox1.Text;
+            var replacement = textBox2.Text;
+
+            var regex = BuildSearchPattern(searchText);
+
+            foreach (var word in sourceText.Substring(position).Split(' '))
+            {
+                var resultWord = regex.IsMatch(word)
+                    ? replacement
+                    : word;
+                transformationResultresult.Append($"{resultWord} ");
+            }
+
+            var result = this.radioButton2.Checked
+                ? sourceText.Substring(0, position)
+                : string.Empty;
+
+            ((Text)Owner).InsertReplacedText(result + transformationResultresult.ToString().Trim());
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            IsActionButtonsEnabled();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            IsActionButtonsEnabled();
+        }
+
+        private void IsActionButtonsEnabled()
+        {
+            var isReplacementButtonsEnable = !string.IsNullOrEmpty(textBox1.Text.Trim())
+                                    && !string.IsNullOrEmpty(textBox2.Text.Trim());
+
+            var isNextButtonEnable = !string.IsNullOrEmpty(textBox1.Text.Trim());
+
+            button1.Enabled = isNextButtonEnable;
+            button2.Enabled = isReplacementButtonsEnable;
+            button3.Enabled = isReplacementButtonsEnable;
+        }
+
+        private int GetCursorPosition()
+        {
+            return this.radioButton1.Checked
+                            ? 0
+                            : ((Text)Owner).GetCursorPosition();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var sourceText = ((Text)Owner).GetText();
+            var startPosition = GetCursorPosition();
+            startPosition += ((Text)Owner).GetSelectedTextIndexStart();
+
+            var searchText = textBox1.Text;
+            var replacement = textBox2.Text;
+            var regex = BuildSearchPattern(searchText);
+
+            foreach (var word in sourceText.Substring(startPosition).Split(' '))
+            {
+                if (regex.IsMatch(word))
+                {
+                    ((Text)Owner).ReplaceText(startPosition, word.Length, replacement);
+                    return;
+                }
+
+                startPosition += word.Length;
+            }
+        }
+
+        private Regex BuildSearchPattern(string searchText)
+        {
+            var regIterator = 1;
+            var regPattern = string.Empty;
+            foreach (var searchChar in searchText)
+            {
+                if (searchChar == WILD_CHARACTER)
+                {
+                    regPattern += $"(.)\\{regIterator}*";
+                    regIterator++;
+                }
+                else
+                {
+                    regPattern += searchChar;
+                }
+            }
+
+            return new Regex($"^{regPattern}$");
+        }
+    }
+}
---
+++ RegularReplacement/Text.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RegularReplacement
+{
+    public partial class Text : Form
+    {
+        public Text()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var replaceForm = new Replace();
+            replaceForm.StartPosition = FormStartPosition.CenterParent;
+            replaceForm.Show(this);
+        }
+
+        public string GetText()
+        {
+            return this.richTextBox1.Text;
+        }
+
+        public void InsertReplacedText(string input)
+        {
+            this.richTextBox1.Text = input;
+        }
+
+        public int GetCursorPosition()
+        {
+            return this.richTextBox1.SelectionStart;
+        }
+
+        public void HigthSelectedText(int startPosition, string word)
+        {
+            this.richTextBox1.Select(0, this.richTextBox1.Text.Length);
+            this.richTextBox1.SelectionBackColor = Color.White;
+
+            this.richTextBox1.Find(word, startPosition, RichTextBoxFinds.MatchCase);
+            this.richTextBox1.SelectionBackColor = Color.Yellow;
+        }
+
+        public void ReplaceText(int startPosition, int length, string replacement)
+        {
+            this.richTextBox1.Text = this.richTextBox1.Text
+                                            .Remove(startPosition, length)
+                                            .Insert(startPosition, replacement);
+        }
+
+        public int GetSelectedTextIndexEnd()
+        {
+            return this.richTextBox1.SelectionLength == 0
+                ? 0
+                : this.richTextBox1.SelectionStart + this.richTextBox1.SelectionLength;
+        }
+
+        public int GetSelectedTextIndexStart()
+        {
+            return this.richTextBox1.SelectionLength == 0
+                ? 0
+                : this.richTextBox1.SelectionStart;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
